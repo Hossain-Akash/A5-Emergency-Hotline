b2f193697d50ca86cba8f62193e486113e0ba2bd
Hossain-Akash <akashnil021499@gmail.com>
2025-08-29 02:48:10 +0600

    added call button function

--- script/index.js
+++ script/index.js
@@ -2,6 +2,7 @@ function getElement(id) {
   const element = document.getElementById(id);
   return element;
 }
+
 // heart
 getElement('emergency-box').addEventListener('click', function (e) {
   if (e.target.className.includes('heart')) {
@@ -13,7 +14,7 @@ getElement('emergency-box').addEventListener('click', function (e) {
   }
 });
 
-// copy
+// copy & call
 getElement('emergency-box').addEventListener('click', function (e) {
   if (e.target.className.includes('copy-btn')) {
     const copyBtn = e.target;
@@ -32,5 +33,21 @@ getElement('emergency-box').addEventListener('click', function (e) {
       .catch(() => {
         alert('Failed to copy. Please copy manually: ' + emergencyNum);
       });
+  } else if (e.target.className.includes('call-btn')) {
+    const callBtn = e.target;
+    const title = callBtn.parentNode.parentNode.children[1].innerText;
+    const subTitle = callBtn.parentNode.parentNode.children[2].innerText;
+    const number = callBtn.parentNode.parentNode.children[3].innerText;
+    const coin = getElement('coin').innerText;
+
+    if (coin <= 0) {
+      alert('❌You Do Not Have Sufficient Coin. At Least Need 20 coins');
+      return;
+    }
+
+    let currentCoin = Number(coin) - 20;
+    getElement('coin').innerText = currentCoin;
+
+    alert('📞Calling ' + subTitle + '' + number);
   }
 });
